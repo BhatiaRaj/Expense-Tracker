7f4ffa911e5ef7f557ee9b55b666383c3cc4aa4a
Raj Bhatia <97431540+BhatiaRaj@users.noreply.github.com>
2022-10-31 17:19:25 +0530

    First Commit

---
+++ Server/routes/route.js
@@ -0,0 +1,21 @@
+const routes = require('express').Router();
+const controller=require('../controller/controller');
+
+
+// creating routes
+
+routes.route('/api/categories')
+    .post(controller.create_Categories)
+    .get(controller.get_Category)
+    
+
+// export server using module
+
+routes.route('/api/transaction')
+   .post(controller.create_Transaction)
+   .get(controller.get_Transaction)
+   
+routes.route('/api/labels')
+   .get(controller.get_Labels)
+
+module.exports=routes;
